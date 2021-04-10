cae993f9bd6340cc3287c2a99d76f60d7947213b
sivan <sxwxiong@163.com>
2021-04-11 02:36:35 +0800

    括号问题

---
+++ src/findSpace.go
@@ -0,0 +1,34 @@
+package leetcode
+
+func isValid(s string) bool {
+	// write code here
+	if len(s) < 2 {
+		return false
+	}
+	stack := make([]byte, 0)
+	for i := 0; i < len(s); i++ {
+		if s[i] == '(' || s[i] == '[' || s[i] == '}' {
+			stack = append(stack, s[i])
+		} else {
+			if len(stack) < 1 {
+				return false
+			}
+			switch s[i] {
+			case ')':
+				if stack[len(stack)-1] != ')' {
+					return false
+				}
+			case ']':
+				if stack[len(stack)-1] != ']' {
+					return false
+				}
+			case '}':
+				if stack[len(stack)-1] != '}' {
+					return false
+				}
+			}
+			stack = stack[:len(stack)-1]
+		}
+	}
+	return true
+}
